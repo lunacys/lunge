954217d40a204632f5b8d5ed74e8282852180672
lunacys <loonacuse@gmail.com>
2018-09-18 16:13:34 +0500

    Add game system mechanism

---
+++ lunge.Library/GameSystems/GameSystemAddedEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lunge.Library.GameSystems
+{
+    public class GameSystemAddedEventArgs : EventArgs
+    {
+        public IGameSystemManager GameSystemManager { get; }
+        public GameSystem GameSystem { get; }
+
+        public GameSystemAddedEventArgs(IGameSystemManager gameSystemManager, GameSystem gameSystem)
+        {
+            GameSystemManager = gameSystemManager;
+            GameSystem = gameSystem;
+        }
+    }
+}
---
+++ lunge.Library/GameSystems/IGameSystemManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace lunge.Library.GameSystems
+{
+    public interface IGameSystemManager
+    {
+        /// <summary>
+        /// Finds and returns <see cref="GameSystem"/> with specified type
+        /// </summary>
+        /// <typeparam name="T">Type of the system</typeparam>
+        /// <returns><see cref="GameSystem"/></returns>
+        T FindSystem<T>() where T : GameSystem;
+
+        /// <summary>
+        /// Gets all registered game systems
+        /// </summary>
+        /// <returns>All registered game systems</returns>
+        IList<GameSystem> GetAllGameSystems();
+    }
+}
