624fd67fd4faaf7b500b1c73b406c33f7c706a03
lunacys <loonacuse@gmail.com>
2018-09-18 20:23:54 +0500

    Done separating GameSystem

--- lunge.Library/GameSystems/DrawableGameSystem.cs
+++ lunge.Library/GameSystems/DrawableGameSystem.cs
@@ -1,42 +1,22 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace lunge.Library.GameSystems
 {
     public abstract class DrawableGameSystem : GameSystem
     {
-        public event EventHandler OnReset;
-
-        protected DrawableGameSystem()
-        { }
-
         /// <summary>
-        /// Gets <see cref="IGameSystemManager"/>
+        /// Gets <see cref="Microsoft.Xna.Framework.Graphics.SpriteBatch"/> which is currently in use
         /// </summary>
-        public IGameSystemManager GameSystemManager { get; internal set; }
+        public SpriteBatch SpriteBatch { get; }
 
-        /// <summary>
-        /// Gets or sets whether the game system is working
-        /// </summary>
-        public bool IsWorking { get; set; }
-
-        /// <summary>
-        /// Finds and returns <see cref="DrawableGameSystem"/> with specified type
-        /// </summary>
-        /// <typeparam name="T">Type of the system</typeparam>
-        /// <returns><see cref="DrawableGameSystem"/></returns>
-        public T FindSystem<T>() where T : DrawableGameSystem
+        protected DrawableGameSystem(GraphicsDevice graphicsDevice)
         {
-            return GameSystemManager?.FindSystem<T>();
+            SpriteBatch = new SpriteBatch(graphicsDevice);
         }
         
-        public virtual void Initialize() { }
-        public virtual void Update(GameTime gameTime) { }
         public virtual void Draw(GameTime gameTime) { }
 
-        public virtual void Reset()
-        {
-            OnReset?.Invoke(this, EventArgs.Empty);
-        }
     }
 }
--- lunge.Library/GameSystems/GameSystem.cs
+++ lunge.Library/GameSystems/GameSystem.cs
@@ -1,15 +1,38 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
 
 namespace lunge.Library.GameSystems
 {
     public abstract class GameSystem : IDisposable
     {
+        public event EventHandler OnReset;
 
+        /// <summary>
+        /// Gets <see cref="IGameSystemManager"/>
+        /// </summary>
+        public IGameSystemManager GameSystemManager { get; internal set; }
 
-        protected virtual void Dispose(bool disposing)
+        /// <summary>
+        /// Gets or sets whether the game system is working
+        /// </summary>
+        public bool IsWorking { get; set; }
+
+        protected GameSystem() { }
+
+        /// <summary>
+        /// Finds and returns <see cref="DrawableGameSystem"/> with specified type
+        /// </summary>
+        /// <typeparam name="T">Type of the system</typeparam>
+        /// <returns><see cref="DrawableGameSystem"/></returns>
+        public T FindSystem<T>() where T : DrawableGameSystem
         {
-            
+            return GameSystemManager?.FindSystem<T>();
         }
+
+        public virtual void Initialize() { }
+        public virtual void Update(GameTime gameTime) { }
+
+        protected virtual void Dispose(bool disposing) { }
 
         public void Dispose()
         {
