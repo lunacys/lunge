5952de80feda406148fbd7c41a30eb8505143f6a
lunacys <loonacuse@gmail.com>
2018-12-04 13:56:12 +0500

    Fix build - Implement all missing methods

--- lunge.Library/Entities/EntityManager.cs
+++ lunge.Library/Entities/EntityManager.cs
@@ -1,13 +1,36 @@
-﻿using lunge.Library.Entities.Systems;
+﻿using System;
+using lunge.Library.Entities.Systems;
 using Microsoft.Xna.Framework;
 
 namespace lunge.Library.Entities
 {
     public class EntityManager : UpdateSystem
     {
+        public int ActiveCount => throw new System.NotImplementedException();
+
         public override void Update(GameTime gameTime)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
+        }
+
+        public Entity Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entity Get(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Destroy(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Destroy(Entity entity)
+        {
+            throw new NotImplementedException();
         }
     }
 }
