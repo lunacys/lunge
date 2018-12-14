925576f0be6fe47a510aa4a8a2f406fa1cea7122
lunacys <ilunacys@gmail.com>
2018-12-14 19:42:39 +0500

    Add MathUtils tests

---
+++ Source/Tests/lunge.Library.Tests/MathUtils_Test.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.Xna.Framework;
+using NUnit.Framework;
+
+namespace lunge.Library.Tests
+{
+    public class MathUtils_Test
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void InBetween_Test()
+        {
+            Assert.AreEqual(MathUtils.InBetween(100f, 50f, 0f, 100f), 50.0f);
+            Assert.AreEqual(MathUtils.InBetween(200f, 50f, 0f, 100f), 100.0f);
+        }
+
+        [Test]
+        public void FromPolar_Test()
+        {
+            // Length tests
+            Assert.AreEqual(MathUtils.FromPolar(0, 10).Length(), 10);
+            Assert.AreEqual(MathUtils.FromPolar((float)Math.PI/2.0f, 10).Length(), 10);
+
+            // Direction tests
+            Assert.AreEqual(MathUtils.FromPolar(0, 10), new Vector2(10, 0));
+            Assert.AreEqual(MathUtils.FromPolar((float)Math.PI, 10).X, new Vector2(-10, 0).X);
+        }
+    }
+}
