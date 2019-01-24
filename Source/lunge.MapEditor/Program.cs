a264fb32ac39fe3bcd40851eba60e1fbf70310bc
lunacys <loonacuse@gmail.com>
2019-01-24 22:07:38 +0500

    Add Map Editor template

---
+++ Source/lunge.MapEditor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lunge.MapEditor
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameRoot())
+                game.Run();
+        }
+    }
+}
