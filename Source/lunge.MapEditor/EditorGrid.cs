b31a424fbf6d4dd19014157dcfbe8ca67d77dc52
lunacys <loonacuse@gmail.com>
2019-01-26 18:29:37 +0500

    Add map editor template project

---
+++ Source/lunge.MapEditor/EditorGrid.cs
@@ -0,0 +1,23 @@
+﻿namespace lunge.MapEditor
+{
+    public class EditorGrid
+    {
+        public int TileWidth { get; set; }
+        public int TileHeight { get; set; }
+        public int GridWidth { get; set; }
+        public int GridHeight { get; set; }
+
+        public EditorGrid(int gridWidth, int gridHeight, int tileWidth, int tileHeight)
+        {
+            TileWidth = tileWidth;
+            TileHeight = tileHeight;
+            GridWidth = gridWidth;
+            GridHeight = gridHeight;
+        }
+
+        public void SetTile(int x, int y, int tileId)
+        {
+            
+        }
+    }
+}
---
+++ Source/lunge.MapEditor/Screens/MainScreen.cs
@@ -0,0 +1,14 @@
+﻿using lunge.Library.Screens;
+using Microsoft.Xna.Framework;
+
+namespace lunge.MapEditor.Screens
+{
+    public class MainScreen : Screen
+    {
+        public MainScreen(Game game)
+            : base(game)
+        {
+
+        }
+    }
+}
