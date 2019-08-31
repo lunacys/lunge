f6e5753bb831fb3f1f1c4b5e701aafcdf8c0a4bb
lunacys <loonacuse@gmail.com>
2019-08-31 18:58:37 +0500

    [GUI] Add Image control

---
+++ Source/lunge.Library/Gui/Controls/Image.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using lunge.Library.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+
+namespace lunge.Library.Gui.Controls
+{
+    public class Image : ControlBase, IGraphicsControl
+    {
+        public Vector2 Position { get; set; }
+        public Size2 Size
+        {
+            get
+            {
+                var width = Sprite?.Width ?? 0;
+                var height = Sprite?.Height ?? 0;
+
+                return new Size2(width, height);
+            }
+            set => throw new NotImplementedException();
+        }
+        public Sprite Sprite { get; set; }
+
+        public Image(string name, Sprite sprite, IControl parentControl = null)
+            : base(name, parentControl)
+        {
+            Sprite = sprite;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Sprite, Position);
+        }
+    }
+}
