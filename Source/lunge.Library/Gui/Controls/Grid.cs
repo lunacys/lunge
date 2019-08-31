5b363e853483a490b052c576a8b26e87d10bd5d8
lunacys <loonacuse@gmail.com>
2019-08-31 18:59:32 +0500

    [GUI] Add Grid control

---
+++ Source/lunge.Library/Gui/Controls/Grid.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using lunge.Library.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+
+namespace lunge.Library.Gui.Controls
+{
+    public class Grid : ControlBase, IGraphicsControl
+    {
+        private Vector2 _position;
+        public Vector2 Position
+        {
+            get => _position;
+            set
+            {
+                _position = value;
+                InitializeCells();
+            }
+        }
+        public Size2 Size { get; set; }
+        public Size2 CellSize { get; set; }
+        public float BorderWidth { get; set; }
+
+        public GridCell[,] Cells { get; private set; }
+
+        public Grid(string name, Size2 gridSize, Size2 cellSize, IControl parentControl = null) 
+            : base(name, parentControl)
+        {
+            Size = gridSize;
+            CellSize = cellSize;
+            
+            var width = (int)Math.Floor(gridSize.Width / cellSize.Width);
+            var height = (int)Math.Floor(gridSize.Height / cellSize.Height);
+            Cells = new GridCell[width, height];
+
+            Position = Vector2.Zero;
+            BorderWidth = 1.0f;
+        }
+
+        private void InitializeCells()
+        {
+            for (int i = 0; i < Cells.GetLength(0); i++)
+            {
+                for (int j = 0; j < Cells.GetLength(1); j++)
+                {
+                    var position = new Vector2(Position.X + i * CellSize.Width, Position.Y + j * CellSize.Height);
+                    Cells[i, j] = new GridCell(position, CellSize);
+                }
+            }
+        }
+
+        public GridCell GetCellAt(int x, int y)
+        {
+            return Cells[x, y];
+        }
+
+        public void ForEach(Action<GridCell, int, int> action)
+        {
+            for (int i = 0; i < Cells.GetLength(0); i++)
+            {
+                for (int j = 0; j < Cells.GetLength(1); j++)
+                {
+                    action(Cells[i, j], i, j);
+                }
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < Cells.GetLength(0); i++)
+            {
+                for (int j = 0; j < Cells.GetLength(1); j++)
+                {
+                    Cells[i, j].Draw(spriteBatch);
+                }
+            }
+
+            spriteBatch.DrawRectangle(Position, Size, Color.Black, BorderWidth);
+        }
+    }
+}
---
+++ Source/lunge.Library/Gui/Controls/GridCell.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+
+namespace lunge.Library.Gui.Controls
+{
+    public class GridCell
+    {
+        public Color BackgroundColor { get; set; }
+        public Color ForegroundColor { get; set; }
+        public Color BorderColor { get; set; }
+        public float BorderWidth { get; set; }
+        public string Text { get; set; }
+        public Image Image { get; set; }
+        public Vector2 Position { get; }
+        public Size2 Size { get; }
+        public Rectangle BoundingRectangle { get; }
+        public SpriteFont Font { get; set; }
+        public TextAlignment TextAlign { get; set; }
+
+        private IGraphicsControl _attachedControl;
+        public IGraphicsControl AttachedControl
+        {
+            get => _attachedControl;
+            set
+            {
+                _attachedControl = value;
+                _attachedControl.Position = Position;
+                _attachedControl.Size = Size;
+            }
+        }
+
+        internal GridCell(Vector2 position, Size2 size)
+        {
+            BackgroundColor = Color.White;
+            ForegroundColor = Color.Black;
+            BorderColor = Color.Black;
+            Text = "";
+            Image = null;
+            BorderWidth = 1.0f;
+            TextAlign = TextAlignment.Center;
+
+            Position = position;
+            Size = size;
+            BoundingRectangle = new Rectangle(Position.ToPoint(), new Point((int)Size.Width, (int)Size.Height));
+        }
+
+        internal void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.FillRectangle(Position, Size, BackgroundColor);
+
+            if (Image != null)
+            {
+                spriteBatch.Draw(Image.Sprite, BoundingRectangle);
+            }
+
+            if (!string.IsNullOrEmpty(Text))
+            {
+                if (Font == null)
+                    throw new NullReferenceException("Set Font property first.");
+
+                spriteBatch.DrawString(Font, Text, BoundingRectangle, ForegroundColor, TextAlign);
+            }
+            
+            spriteBatch.DrawRectangle(Position, Size, BorderColor, BorderWidth);
+        }
+    }
+}
