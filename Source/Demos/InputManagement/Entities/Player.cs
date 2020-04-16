5f55ea2503049700e32f738417c68fb0b5131460
Grigory Alfyorov <loonacuse@gmail.com>
2020-04-16 20:13:09 +0500

    Add InputManagement demo

---
+++ Source/Demos/InputManagement/Entities/Player.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using lunge.Library.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace InputManagement.Entities
+{
+    public class JumpCommand : IInputCommand<Player>
+    {
+        public void Execute(Player entity)
+        {
+            Console.WriteLine("Jumping!");
+        }
+    }
+
+    public enum PlayerDirection
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    }
+
+    public class MoveCommand : IInputCommand<Player>
+    {
+        public PlayerDirection Direction { get; }
+        public float Speed { get; set; }
+
+        public MoveCommand(PlayerDirection direction, float speed)
+        {
+            Direction = direction;
+            Speed = speed;
+        }
+
+        public void Execute(Player entity)
+        {
+            switch (Direction)
+            {
+                case PlayerDirection.Up:
+                    entity.Position.Y -= Speed;
+                    break;
+                case PlayerDirection.Right:
+                    entity.Position.X += Speed;
+                    break;
+                case PlayerDirection.Down:
+                    entity.Position.Y += Speed;
+                    break;
+                case PlayerDirection.Left:
+                    entity.Position.X -= Speed;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    public class Player : IInputHandleable
+    {
+        public Texture2D Texture { get; }
+        public Vector2 Position;
+        public InputHandler<Keys, Player> _handler;
+        public float MovementSpeed { get; set; }
+
+        public Player(Texture2D texture)
+        {
+            Texture = texture;
+            MovementSpeed = 4.0f;
+            _handler = new InputHandler<Keys, Player>();
+
+            _handler.Register(
+                new InputEntry<Keys>(Keys.Space, InputManager.WasKeyPressed),
+                new JumpCommand()
+            );
+            _handler.Register(
+                new InputEntry<Keys>(Keys.W, InputManager.IsKeyDown),
+                new MoveCommand(PlayerDirection.Up, MovementSpeed)
+            );
+            _handler.Register(
+                new InputEntry<Keys>(Keys.D, InputManager.IsKeyDown),
+                new MoveCommand(PlayerDirection.Right, MovementSpeed)
+            );
+            _handler.Register(
+                new InputEntry<Keys>(Keys.S, InputManager.IsKeyDown),
+                new MoveCommand(PlayerDirection.Down, MovementSpeed)
+            );
+            _handler.Register(
+                new InputEntry<Keys>(Keys.A, InputManager.IsKeyDown),
+                new MoveCommand(PlayerDirection.Left, MovementSpeed)
+            );
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // TODO: Check high CPU usage. Probably because of 'yield return'
+            foreach (var cmd in _handler.Handle())
+            {
+                cmd.Execute(this);
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, Color.White);
+        }
+    }
+}
---
+++ Source/Demos/InputManagement/GameRoot.cs
@@ -0,0 +1,67 @@
+﻿using InputManagement.Entities;
+using lunge.Library.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+
+namespace InputManagement
+{
+    public class GameRoot : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Player _player;
+        private FramesPerSecondCounterComponent _fpsCounter;
+
+        public GameRoot()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _fpsCounter = new FramesPerSecondCounterComponent(this);
+            Components.Add(_fpsCounter);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            _player = new Player(Content.Load<Texture2D>("Player"));
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            InputManager.Update(gameTime);
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            Window.Title = $"{_fpsCounter.FramesPerSecond}";
+            _player.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _player.Draw(_spriteBatch);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
