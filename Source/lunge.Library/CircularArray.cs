f0abc0e8c7a066daae7cdaf81e8f385ec9ebef1d
Grigory Alfyorov <loonacuse@gmail.com>
2020-04-17 19:42:05 +0500

    Improve FramesPerSecondCounterComponent performance

---
+++ Source/lunge.Library/CircularArray.cs
@@ -0,0 +1,29 @@
+﻿namespace lunge.Library
+{
+    public class CircularArray<T>
+    {
+        private int _start;
+
+        public int Start
+        {
+            get => _start;
+            set => _start = value % _array.Length;
+        }
+
+        public int Count { get; set; }
+        public int Capacity => _array.Length;
+
+        private readonly T[] _array;
+
+        public CircularArray(int capacity)
+        {
+            _array = new T[capacity];    
+        }
+
+        public T this[int index]
+        {
+            get => _array[(_start + index) % _array.Length];
+            set => _array[(_start + index) % _array.Length] = value;
+        }
+    }
+}
