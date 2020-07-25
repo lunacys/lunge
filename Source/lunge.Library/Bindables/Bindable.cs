236ef89a15545e795e454702c646237f92703101
loonacuse <loonacuse@gmail.com>
2020-07-26 00:11:33 +0500

    Add Bindable values: double, float, int, List<T>, Vector2

---
+++ Source/lunge.Library/Bindables/Bindable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace lunge.Library.Bindables
+{
+    public class Bindable<T> : IDisposable
+    {
+        public EventHandler<BindableValueChangeEvent<T>> ValueChanged;
+        
+        public string Name { get; }
+        public T Default { get; set; }
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                var oldVal = _value;
+                _value = value;
+                ValueChanged?.Invoke(this, new BindableValueChangeEvent<T>(value, oldVal));
+            }
+        }
+
+        private T _value;
+
+        public Bindable(T defaultVal, EventHandler<BindableValueChangeEvent<T>> action = null)
+        {
+            if (action != null)
+                ValueChanged += action;
+
+            Default = defaultVal;
+            Value = defaultVal;
+        }
+
+        public Bindable(string name, T defaultVal, EventHandler<BindableValueChangeEvent<T>> action = null)
+        {
+            if (action != null)
+                ValueChanged += action;
+
+            Name = name;
+            Default = defaultVal;
+            Value = Default;
+        }
+        
+        public void UnHookEventHandlers() => ValueChanged = null;
+        public override string ToString() => Value.ToString();
+        public void Dispose() => UnHookEventHandlers();
+        public void TriggerChange() => ValueChanged?.Invoke(this, new BindableValueChangeEvent<T>(Value, Value));
+        public void ChangeWithoutTrigger(T val) => _value = val;
+    }
+}
---
+++ Source/lunge.Library/Bindables/BindableInt.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace lunge.Library.Bindables
+{
+    public class BindableInt : Bindable<int>
+    {
+        /// <summary>
+        ///     The mininimum value that it will be clamped to.
+        /// </summary>
+        public int MinValue { get; }
+
+        /// <summary>
+        ///     The maximum value that it will be clamped to
+        /// </summary>
+        public int MaxValue { get; }
+
+        /// <summary>
+        ///     The value of this BindedInt
+        /// </summary>
+        private int _value;
+
+        public new int Value
+        {
+            get => _value;
+            set
+            {
+                var previousVal = _value;
+                _value = MathHelper.Clamp(value, MinValue, MaxValue);
+
+                if (_value != previousVal)
+                    ValueChanged?.Invoke(this, new BindableValueChangeEvent<int>(_value, previousVal));
+            }
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="defaultVal"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <param name="action"></param>
+        public BindableInt(int defaultVal, int min, int max, EventHandler<BindableValueChangeEvent<int>> action = null)
+            : base(defaultVal, action)
+        {
+            MinValue = min;
+            MaxValue = max;
+            Value = defaultVal;
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="defaultVal"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <param name="action"></param>
+        public BindableInt(string name, int defaultVal, int min, int max,
+            EventHandler<BindableValueChangeEvent<int>> action = null)
+            : base(name, defaultVal, action)
+        {
+            MinValue = min;
+            MaxValue = max;
+            Value = defaultVal;
+        }
+
+        /// <summary>
+        ///     Manually triggers the event that the bindable has changed. Normally this won't happen if the old value
+        ///     and new value are the same
+        /// </summary>
+        public void TriggerChangeEvent() => ValueChanged?.Invoke(this, new BindableValueChangeEvent<int>(Value, Value));
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Prints a stringified value of the Bindable.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() => Value.ToString();
+    }
+}
