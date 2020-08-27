7b327ddac21001521dc57875749403324375a8a1
loonacuse <loonacuse@gmail.com>
2020-08-27 17:01:08 +0500

    Add Scheduling from Wobble

---
+++ src/lunge.Library/Scheduling/TaskCancelledEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lunge.Library.Scheduling
+{
+    public class TaskCancelledEventArgs<T> : EventArgs
+    {
+        /// <summary>
+        ///     Input given to task before cancelled
+        /// </summary>
+        public T Input { get; }
+
+        public TaskCancelledEventArgs(T input) => Input = input;
+    }
+}
---
+++ src/lunge.Library/Scheduling/TaskCompletedEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lunge.Library.Scheduling
+{
+    public class TaskCompletedEventArgs<T, TResult> : EventArgs
+    {
+        /// <summary>
+        ///     Input given to task
+        /// </summary>
+        public T Input { get; }
+
+        /// <summary>
+        ///     Result of the task
+        /// </summary>
+        public TResult Result { get; }
+
+        public TaskCompletedEventArgs(T input, TResult result)
+        {
+            Input = input;
+            Result = result;
+        }
+    }
+}
